e532d505ada9e8223b66ff2c9f7ba9a3f11f15c3
charlielito <candres.alv@gmail.com>
2020-07-11 16:18:32 -0500

    remove unsued moethods

--- elegy/model.py
+++ elegy/model.py
@@ -917,39 +917,3 @@ class Model(object):
 
     _predict_jit = jax.jit(_predict, static_argnums=(0,))
 
-    @property
-    def metrics_names(self):
-        """Returns the model's display labels for all outputs.
-        Note: `metrics_names` are available only after a `keras.Model` has been
-        trained/evaluated on actual data.
-        Examples:
-        >>> inputs = tf.keras.layers.Input(shape=(3,))
-        >>> outputs = tf.keras.layers.Dense(2)(inputs)
-        >>> model = tf.keras.models.Model(inputs=inputs, outputs=outputs)
-        >>> model.compile(optimizer="Adam", loss="mse", metrics=["mae"])
-        >>> model.metrics_names
-        []
-        >>> x = np.random.random((2, 3))
-        >>> y = np.random.randint(0, 2, (2, 2))
-        >>> _ = model.fit(x, y, verbose=0)
-        >>> model.metrics_names
-        ['loss', 'mae']
-        >>> inputs = tf.keras.layers.Input(shape=(3,))
-        >>> d = tf.keras.layers.Dense(2, name='out')
-        >>> output_1 = d(inputs)
-        >>> output_2 = d(inputs)
-        >>> model = tf.keras.models.Model(
-        ...    inputs=inputs, outputs=[output_1, output_2])
-        >>> model.compile(optimizer="Adam", loss="mse", metrics=["mae", "acc"])
-        >>> _ = model.fit(x, (y, y), verbose=0)
-        >>> model.metrics_names
-        ['loss', 'out_loss', 'out_1_loss', 'out_mae', 'out_acc', 'out_1_mae',
-        'out_1_acc']
-        """
-
-        # This property includes all output names including `loss` and per-output
-        # losses for backward compatibility.
-        return [m.name for m in self.metrics]
-
-    def _get_callback_model(self):
-        return self
