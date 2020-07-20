9a376430085183f9f07332060383f099ff2143b4
David Cardozo <david@cerberusdata.ai>
2020-07-19 21:33:14 -0500

    Feature/tensorboard callbacks (#38)
    * [Feat] Adding Tensorboard Callback support.
    
    Also apply to update CUDA dockerfile and have options to run jax GPU enabled.
    
    * [Feat] Adding Tensorboard Callback support.
    
    Also apply to update CUDA dockerfile and have options to run jax GPU enabled.
    
    * [Fix] Typo in documentation
    
    * [Fix] Added to docs ToC
    
    * [Fix] Added to docs ToC
    
    * [Fix] Apply to change names to adopt to conventions.
    
    * [Feat] Simulate Tensorboard api for update_freq
    
    * [Fix] Adding docstring for update_freq
    
    * [Update] Set lock and pyproject info
    
    * [Fix] Missing typo
    
    Co-authored-by: charlielito <candres.alv@gmail.com>

---
+++ elegy/callbacks/tensorboard.py
@@ -0,0 +1,107 @@
+# Implementation based on tf.keras.callbacks.py
+# https://github.com/tensorflow/tensorflow/blob/v2.2.0/tensorflow/python/keras/callbacks.py
+
+
+import collections
+import numpy as np
+import six
+from six.moves import collections_abc
+from tensorboardX.writer import SummaryWriter
+from typing import Optional, Union
+
+from .callback import Callback
+
+
+class TensorBoard(Callback):
+    """Callback that streams epoch results to tensorboard events folder.
+
+Supports all values that can be represented as a string,
+including 1D iterables such as `np.ndarray`.
+
+Example:
+
+    ```python
+    tensorboard_logger = TensorBoard('runs')
+    model.fit(X_train, Y_train, callbacks=[tensorboard_logger])
+    ```
+  """
+
+    def __init__(
+        self,
+        logdir: Optional[str] = None,
+        update_freq: Union[str, int] = "epoch",
+        **kwargs
+    ) -> None:
+        """
+        Arguments:
+             logdir (string): Save directory location. Default is
+              runs/**CURRENT_DATETIME_HOSTNAME**, which changes after each run.
+              Use hierarchical folder structure to compare
+              between runs easily. e.g. pass in 'runs/exp1', 'runs/exp2', etc.
+              for each new experiment to compare across them.
+              update_freq (int or String): `'batch'` or `'epoch'` or integer. When using `'batch'`,
+              writes the losses and metrics to TensorBoard after each batch. The same
+                applies for `'epoch'`. 
+            **kwargs (dict): Options to pass to `SummaryWriter` object
+        """
+        self.logdir = logdir
+        self.writer = None
+        self.keys = None
+        if update_freq == "batch":
+            self.update_freq = 1
+        else:
+            self.update_freq = update_freq
+        self._open_args = kwargs if kwargs else {}
+        super(TensorBoard, self).__init__()
+
+    def on_train_begin(self, logs=None):
+        self.writer = SummaryWriter(self.logdir, **self._open_args)
+
+    def on_train_batch_end(self, batch: int, logs):
+        if self.update_freq == "epoch":
+            return
+        logs = logs or {}
+
+        def handle_value(k):
+            is_zero_dim_ndarray = isinstance(k, np.ndarray) and k.ndim == 0
+            if isinstance(k, six.string_types):
+                return k
+            elif isinstance(k, collections_abc.Iterable) and not is_zero_dim_ndarray:
+                return '"[%s]"' % (", ".join(map(str, k)))
+            else:
+                return k
+
+        if self.update_freq != "epoch" and batch % self.update_freq == 0:
+            if self.keys is None:
+                self.keys = sorted(logs.keys())
+            row_dict = collections.OrderedDict({"batch": batch})
+            row_dict.update(
+                (key + "batch", handle_value(logs[key])) for key in self.keys
+            )
+
+            for key, value in row_dict.items():
+                self.writer.add_scalar(key, value, batch)
+
+    def on_epoch_end(self, epoch, logs=None):
+        logs = logs or {}
+
+        def handle_value(k):
+            is_zero_dim_ndarray = isinstance(k, np.ndarray) and k.ndim == 0
+            if isinstance(k, six.string_types):
+                return k
+            elif isinstance(k, collections_abc.Iterable) and not is_zero_dim_ndarray:
+                return '"[%s]"' % (", ".join(map(str, k)))
+            else:
+                return k
+
+        if self.keys is None:
+            self.keys = sorted(logs.keys())
+
+        row_dict = collections.OrderedDict({"epoch": epoch})
+        row_dict.update((key, handle_value(logs[key])) for key in self.keys)
+
+        for key, value in row_dict.items():
+            self.writer.add_scalar(key, value, epoch)
+
+    def on_train_end(self, logs=None):
+        self.writer.close()
--- examples/mnist_autoencoder.py
+++ examples/mnist_autoencoder.py
@@ -66,7 +66,12 @@ def main(debug: bool = False, eager: bool = False):
 
     # Notice we are not passing `y`
     history = model.fit(
-        x=X_train, epochs=20, batch_size=64, validation_data=(X_test,), shuffle=True,
+        x=X_train,
+        epochs=20,
+        batch_size=64,
+        validation_data=(X_test,),
+        shuffle=True,
+        callbacks=[elegy.callbacks.TensorBoard()],
     )
 
     plot_history(history)
