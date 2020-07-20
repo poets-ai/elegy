09a97a94ca8642432d09e34eb224539a15c6e6b0
Henry Ruiz <henryruiz22@gmail.com>
2020-07-20 10:14:46 -0500

    refactor: binary_cross_entropy function

--- elegy/losses/binary_crossentropy.py
+++ elegy/losses/binary_crossentropy.py
@@ -16,9 +16,16 @@ def binary_cross_entropy(
     avoid ever calculating the log of 0.0. This means that in practice, 
     the best possible loss will be a value very close to zero, but not exactly zero.
     '''
-    score = jnp.sum(y_true * jnp.log(utils.EPSILON + y_pred))
-    mean_sum_score = 1.0 / len(y_true) * score
-    return -jnp.array(mean_sum_score)
+    #score = jnp.sum(y_true * jnp.log(utils.EPSILON + y_pred))
+    #mean_sum_score = 1.0 / len(y_true) * score
+    #return -jnp.array(mean_sum_score)
+    #return -jnp.sum(y_true * jnp.log(y_pred) - (1 - y_true) * jnp.log(1 - y_pred))
+
+    # implementation 2
+    score = jnp.sum(y_true * jnp.log(y_pred)
+                    + (1 - y_true) * jnp.log(1 - y_pred))
+    return jnp.array((-1 / len(y_true)) * score)
+
 
 
 class BinaryCrossEntropy(Loss):
--- elegy/losses/binary_crossentropy_test.py
+++ elegy/losses/binary_crossentropy_test.py
@@ -11,5 +11,16 @@ def test_basic():
     y_pred = jnp.array([[0.6, 0.4], [0.4, 0.6]])
     bce = elegy.losses.BinaryCrossEntropy()
     result = bce(y_true, y_pred)
-    assert jnp.isclose(result, 0.458, rtol=0.01)
+    assert jnp.isclose(result, 1.630, rtol=0.01)
+
+
+if __name__ == '__main__':
+    ground_truth = jnp.array([[0., 1.], [0., 0.]])
+    scores = jnp.array([[0.6, 0.4], [0.4, 0.6]])
+    score = jnp.sum(ground_truth*jnp.log(scores)
+                    + (1-ground_truth)*jnp.log(1-scores))
+    result = (-1 / len(ground_truth)) * score
+    print(result)
+
+
 
