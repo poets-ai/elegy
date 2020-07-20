dd998a6d3bca296d4be756f51e7f668c92a6a397
Sebastian Arango <sebasarango1180@gmail.com>
2020-07-20 17:51:54 -0500

    Added upper (1.0) clipping for non-logit bce

--- elegy/losses/binary_crossentropy.py
+++ elegy/losses/binary_crossentropy.py
@@ -15,7 +15,7 @@ def binary_crossentropy(
     if from_logits:
         return -jnp.mean(y_true * y_pred - jnp.logaddexp(0.0, y_pred), axis=-1)
 
-    y_pred = jnp.maximum(y_pred, utils.EPSILON)
+    y_pred = jnp.clip(y_pred, utils.EPSILON, 1.0 - utils.EPSILON)
     return -jnp.mean(y_true * jnp.log(y_pred) + (1 - y_true) * jnp.log(1 - y_pred), axis=-1)
 
 
