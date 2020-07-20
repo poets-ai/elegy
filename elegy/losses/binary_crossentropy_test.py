cb7638bfe010ec038e7b5d427e188c128bdd8890
Henry Ruiz <henryruiz22@gmail.com>
2020-07-20 12:18:43 -0500

    refactor: tests Binary-Cross-Entropy loss

--- elegy/losses/binary_crossentropy_test.py
+++ elegy/losses/binary_crossentropy_test.py
@@ -8,18 +8,18 @@ def test_basic():
     y_true = jnp.array([[0., 1.], [0., 0.]])
     y_pred = jnp.array([[0.6, 0.4], [0.4, 0.6]])
 
-    bce = elegy.losses.BinaryCrossEntropy()
+    bce = elegy.losses.BinaryCrossentropy()
     result = bce(y_true, y_pred)
     assert jnp.isclose(result, 0.815, rtol=0.01)
 
     result = bce(y_true, y_pred, sample_weight=jnp.array([1, 0]))
     assert jnp.isclose(result, 0.458, rtol=0.01)
 
-    bce = elegy.losses.BinaryCrossEntropy(reduction=elegy.losses.Reduction.SUM)
+    bce = elegy.losses.BinaryCrossentropy(reduction=elegy.losses.Reduction.SUM)
     result = bce(y_true, y_pred)
     assert jnp.isclose(result, 1.630, rtol=0.01)
 
-    bce = elegy.losses.BinaryCrossEntropy(reduction=elegy.losses.Reduction.NONE)
+    bce = elegy.losses.BinaryCrossentropy(reduction=elegy.losses.Reduction.NONE)
     result = bce(y_true, y_pred)
     assert jnp.all(jnp.isclose(result, [0.916, 0.713], rtol=0.01))
 
