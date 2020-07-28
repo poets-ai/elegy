c22e39a7715a33b5f5229c50a0b2acb03eea66a7
Juan Manuel Ciro Torres <jmcirot@unal.edu.co>
2020-07-28 14:52:02 -0500

    Mean Absolute Percentage Error Implementation (#53)

--- elegy/losses/__init__.py
+++ elegy/losses/__init__.py
@@ -1,10 +1,13 @@
-from .loss import Loss, Reduction
+from .binary_crossentropy import BinaryCrossentropy, binary_crossentropy
 from .categorical_crossentropy import CategoricalCrossentropy
+from .loss import Loss, Reduction
+from .mean_absolute_error import MeanAbsoluteError, mean_absolute_error
+from .mean_absolute_percentage_error import (
+    MeanAbsolutePercentageError,
+    mean_percentage_absolute_error,
+)
 from .mean_squared_error import MeanSquaredError, mean_squared_error
 from .sparse_categorical_crossentropy import (
     SparseCategoricalCrossentropy,
     sparse_categorical_crossentropy,
 )
-
-from .mean_absolute_error import MeanAbsoluteError, mean_absolute_error
-from .binary_crossentropy import BinaryCrossentropy, binary_crossentropy
