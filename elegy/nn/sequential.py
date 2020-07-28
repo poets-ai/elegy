063e7b968908fc116c4a324bc7b25aa26173c3e2
Cristian Garcia <cgarcia.e88@gmail.com>
2020-07-28 15:23:40 -0500

    update sequential

--- elegy/nn/sequential.py
+++ elegy/nn/sequential.py
@@ -1,4 +1,5 @@
-from elegy.module import Deferable
+from elegy import utils
+from elegy.module import Deferable, Module
 from elegy import hooks
 import typing as tp
 
@@ -6,9 +7,39 @@ import haiku as hk
 import numpy as np
 
 
-class Sequential(hk.Sequential, Deferable):
+class Sequential(Module):
     """
     Sequentially calls the given list of layers.
+
+    ```python
+    class MLP(elegy.Module):
+        def __apply__(self, x):
+            mlp = elegy.nn.Sequential([
+                elegy.nn.Flatten(),
+                elegy.nn.Linear(100),
+                jax.nn.relu,
+                elegy.nn.Linear(10),
+            ])
+            return mlp(x)
+    ```
+
+    For convenience Elegy's Sequential also accept a lambda as argument which
+    lets you easily pass simple modules to the Model API.
+
+    ```python
+    model = elegy.Model(
+        module=elegy.nn.Sequential.defer(
+            lambda:[
+                elegy.nn.Flatten(),
+                elegy.nn.Linear(100),
+                jax.nn.relu,
+                elegy.nn.Linear(10),
+            ]
+        ),
+        loss=elegy.losses.SparseCategoricalCrossentropy(from_logits=True),
+        optimizer=optix.adam(1e-3),
+    )
+    ```
 
     Note that `Sequential` is limited in the range of possible architectures
     it can handle. This is a deliberate design decision; `Sequential` is only
@@ -34,29 +65,43 @@ class Sequential(hk.Sequential, Deferable):
 
     def __init__(
         self,
-        layers: tp.Iterable[tp.Callable[..., tp.Any]],
+        layers: tp.Union[
+            tp.Iterable[tp.Callable[..., tp.Any]],
+            tp.Callable[[], tp.Iterable[tp.Callable[..., tp.Any]]],
+        ],
         name: tp.Optional[str] = None,
     ):
         """
         Creates a Sequential instance.
 
         Arguments:
-            layers: A list of moules or functions that take a single input.
+            layers: A list of moules or functions that take a single input. You can
+                also pass a cero parameter function of the previous.
             name: An optional string name for the class. Must be a valid Python
                 identifier. If `name` is not provided then the class name for the
                 current instance is converted to `lower_snake_case` and used instead.
         """
-        super().__init__(layers=layers, name=name)
+        super().__init__(name=name)
+        self.layers = layers
 
-    def __call__(self, inputs: np.ndarray, *args, **kwargs):
+    def __apply__(self, inputs: np.ndarray, *args, **kwargs):
         """
+        Connects all layers. *args and **kwargs are passed to the first layer.
+        
         Arguments:
             inputs: Input array.
             args: Additional positional input arguments for the first layer.
             kwargs: Additional keyword arguments for the first layer.
         """
-        outputs = super().__call__(inputs, *args, **kwargs)
 
-        hooks.add_summary(None, self.__class__.__name__, outputs)
+        outputs = inputs
+
+        for i, layer in enumerate(
+            self.layers if isinstance(self.layers, tp.Iterable) else self.layers()
+        ):
+            if i == 0:
+                outputs = utils.inject_dependencies(layer)(outputs, *args, **kwargs)
+            else:
+                outputs = layer(outputs)
 
         return outputs
---
+++ elegy/nn/sequential_test.py
@@ -0,0 +1,42 @@
+import haiku as hk
+import jax.numpy as jnp
+from elegy.testing_utils import transform_and_run
+from unittest import TestCase
+import jax
+
+import elegy
+
+
+class SequentialTest(TestCase):
+    @transform_and_run
+    def test_connects(self):
+        elegy.nn.Sequential(
+            [elegy.nn.Flatten(), elegy.nn.Linear(5), jax.nn.relu, elegy.nn.Linear(2),]
+        )(jnp.ones([10, 3]))
+
+        elegy.nn.Sequential(
+            lambda: [
+                elegy.nn.Flatten(),
+                elegy.nn.Linear(5),
+                jax.nn.relu,
+                elegy.nn.Linear(2),
+            ]
+        )(jnp.ones([10, 3]))
+
+    def test_on_predict(self):
+
+        model = elegy.Model(
+            module=elegy.nn.Sequential.defer(
+                lambda: [
+                    elegy.nn.Flatten(),
+                    elegy.nn.Linear(5),
+                    jax.nn.relu,
+                    elegy.nn.Linear(2),
+                ]
+            )
+        )
+
+        x = jnp.ones([3, 5])
+
+        y_pred = model.predict(x)
+        logs = model.evaluate(x)
