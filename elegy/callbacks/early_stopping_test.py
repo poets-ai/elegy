61fea8e0224f80cfb9b1324ad54316e11264a690
Cristian Garcia <cgarcia.e88@gmail.com>
2020-08-06 15:39:10 -0500

    fix early stopping test

--- elegy/callbacks/early_stopping_test.py
+++ elegy/callbacks/early_stopping_test.py
@@ -4,18 +4,20 @@ from unittest import TestCase
 
 import numpy as np
 from jax.experimental import optix
+import jax
 
 import elegy
 
 np.random.seed(42)
 
 
-class ArrayDataAdapterTest(TestCase):
+class EarlyStoppingTest(TestCase):
     def test_example(self):
         class MLP(elegy.Module):
-            def call(self, input):
-                mlp = elegy.nn.Sequential(lambda: [elegy.nn.Linear(10)])
-                return mlp(input)
+            def call(self, x):
+                x = elegy.nn.Linear(10)(x)
+                x = jax.lax.stop_gradient(x)
+                return x
 
         callback = elegy.callbacks.EarlyStopping(monitor="loss", patience=3)
         # This callback will stop the training when there is no improvement in
@@ -26,11 +28,11 @@ class ArrayDataAdapterTest(TestCase):
             optimizer=optix.rmsprop(0.01),
         )
         history = model.fit(
-            np.arange(100).reshape(5, 20).astype(np.float32),
-            np.zeros(5),
+            np.ones((5, 20)),
+            np.zeros((5, 10)),
             epochs=10,
             batch_size=1,
             callbacks=[callback],
             verbose=0,
         )
-        assert len(history.history["loss"]) == 7  # Only 7 epochs are run.
+        assert len(history.history["loss"]) == 4  # Only 4 epochs are run.
