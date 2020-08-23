ident error in example fix and np.isclose use to prevent precision mistake

elegy/metrics/precision.py
@@ -28,7 +28,7 @@ class Precision(Mean):
     Use `sample_weight` of 0 to mask values.
 
     ```python
-    precision = elegy.metrics.Precision()
+        precision = elegy.metrics.Precision()
 
         result = precision(
             y_true=jnp.array([0, 1, 1, 1]), y_pred=jnp.array([1, 0, 1, 1])
@@ -44,7 +44,7 @@ class Precision(Mean):
     Usage with elegy API:
 
     ```python
-    model = elegy.Model(
+        model = elegy.Model(
         module_fn,
         loss=elegy.losses.CategoricalCrossentropy(),
         metrics=elegy.metrics.Precision(),

elegy/metrics/precision_test.py
@@ -3,6 +3,7 @@ import elegy
 
 from elegy.testing_utils import transform_and_run
 import jax.numpy as jnp
+import numpy as np
 
 
 class PrecisionTest(unittest.TestCase):
@@ -14,12 +15,12 @@ class PrecisionTest(unittest.TestCase):
         result = precision(
             y_true=jnp.array([0, 1, 1, 1]), y_pred=jnp.array([1, 0, 1, 1])
         )
-        assert result == 0.6666667
+        assert np.isclose(result, 0.6666667)
 
         result = precision(
             y_true=jnp.array([1, 1, 1, 1]), y_pred=jnp.array([1, 1, 0, 0])
         )
-        assert result == 0.8
+        assert np.isclose(result, 0.8)
 
         result = precision(
             y_true=jnp.array(
@@ -29,7 +30,7 @@ class PrecisionTest(unittest.TestCase):
                 [[1, 0, 1, 1], [1, 0, 1, 1], [0, 0, 0, 1], [1, 1, 1, 1], [1, 0, 0, 1]]
             ),
         )
-        assert result == 0.5555556
+        assert np.isclose(result, 0.5555556)
 
         result = precision(
             y_true=jnp.array(
@@ -45,4 +46,4 @@ class PrecisionTest(unittest.TestCase):
                 ]
             ),
         )
-        assert result == 0.5
+        assert np.isclose(result, 0.5)

elegy/metrics/recall.py
@@ -28,7 +28,7 @@ class Recall(Mean):
     Use `sample_weight` of 0 to mask values.
 
     ```python
-    recall = elegy.metrics.Recall()
+        recall = elegy.metrics.Recall()
 
         result = recall(
             y_true=jnp.array([0, 1, 1, 1]), y_pred=jnp.array([1, 0, 1, 1])
@@ -44,7 +44,7 @@ class Recall(Mean):
     Usage with elegy API:
 
     ```python
-    model = elegy.Model(
+        model = elegy.Model(
         module_fn,
         loss=elegy.losses.CategoricalCrossentropy(),
         metrics=elegy.metrics.Recall(),

elegy/metrics/recall_test.py
@@ -3,6 +3,7 @@ import elegy
 
 from elegy.testing_utils import transform_and_run
 import jax.numpy as jnp
+import numpy as np
 
 
 class RecallTest(unittest.TestCase):
@@ -12,10 +13,10 @@ class RecallTest(unittest.TestCase):
         recall = elegy.metrics.Recall()
 
         result = recall(y_true=jnp.array([0, 1, 1, 1]), y_pred=jnp.array([1, 0, 1, 1]))
-        assert result == 0.6666667
+        assert np.isclose(result, 0.6666667)
 
         result = recall(y_true=jnp.array([1, 1, 1, 1]), y_pred=jnp.array([1, 0, 0, 0]))
-        assert result == 0.42857143
+        assert np.isclose(result, 0.42857143)
 
         result = recall(
             y_true=jnp.array(
@@ -25,7 +26,7 @@ class RecallTest(unittest.TestCase):
                 [[1, 0, 1, 1], [1, 0, 1, 1], [0, 0, 0, 1], [1, 1, 1, 1], [1, 0, 0, 1]]
             ),
         )
-        assert result == 0.5294118
+        assert np.isclose(result, 0.5294118)
 
         result = recall(
             y_true=jnp.array(
@@ -41,4 +42,4 @@ class RecallTest(unittest.TestCase):
                 ]
             ),
         )
-        assert result == 0.5555556
+        assert np.isclose(result, 0.5555556)