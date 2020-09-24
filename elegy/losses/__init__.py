e077e0411dd07a8f2b52f88168475a0e9b66444e
Abhinav <abhinavsp0730@gmail.com>
2020-09-25 01:01:24 +0530

    Update __init__.py

--- elegy/losses/__init__.py
+++ elegy/losses/__init__.py
@@ -7,6 +7,7 @@ from .mean_absolute_percentage_error import (
     mean_percentage_absolute_error,
 )
 from .mean_squared_error import MeanSquaredError, mean_squared_error
+from .mean_squared_logarithmic_error import MeanSquaredLogarithmicError, mean_squared_logarithmic_error
 from .sparse_categorical_crossentropy import (
     SparseCategoricalCrossentropy,
     sparse_categorical_crossentropy,
@@ -24,6 +25,8 @@ __all__ = [
     "mean_percentage_absolute_error",
     "MeanSquaredError",
     "mean_squared_error",
+    "MeanSquaredLogarithmicError"
+    "mean_squared_logarithmic_error"
     "SparseCategoricalCrossentropy",
     "sparse_categorical_crossentropy",
 ]
