3dd9bcb39ecd1a5eed422519f673fad46342df28
anvelezec <anvelezec@unal.edu.co>
2020-11-21 12:13:20 -0500

    back applied

--- elegy/metrics/f1_test.py
+++ elegy/metrics/f1_test.py
@@ -5,25 +5,26 @@ import tensorflow_addons as tfa
 import numpy as np
 import elegy
 
+
 class F1Test(TestCase):
     #
     def test_basic(self):
 
-        y_true=jnp.array([0, 1, 1, 1])
-        y_pred=jnp.array([1, 0, 1, 1])
+        y_true = jnp.array([0, 1, 1, 1])
+        y_pred = jnp.array([1, 0, 1, 1])
 
         assert np.allclose(
             elegy.metrics.F1()(y_true, y_pred),
-            tfa.metrics.F1Score(2, average="micro", threshold=0.5)(y_true, y_pred)
-                            )  # 2 * (0.44445 / 1.33334)
+            tfa.metrics.F1Score(2, average="micro", threshold=0.5)(y_true, y_pred),
+        )  # 2 * (0.44445 / 1.33334)
 
-        y_true=jnp.array([1, 1, 1, 1])
-        y_pred=jnp.array([1, 1, 0, 0])
-      
+        y_true = jnp.array([1, 1, 1, 1])
+        y_pred = jnp.array([1, 1, 0, 0])
+
         assert np.allclose(
             elegy.metrics.F1()(y_true, y_pred),
-                         tfa.metrics.F1Score(2, average="micro", threshold=0.5)(y_true, y_pred)
-                         )  # 2 * (0.5 / 1.5)
+            tfa.metrics.F1Score(2, average="micro", threshold=0.5)(y_true, y_pred),
+        )  # 2 * (0.5 / 1.5)
 
         y_true = (np.random.uniform(0, 1, size=(5, 6, 7)) > 0.5).astype(np.float32)
         y_pred = np.random.uniform(0, 1, size=(5, 6, 7))
@@ -33,13 +34,13 @@ class F1Test(TestCase):
 
         assert np.allclose(
             tfa.metrics.F1Score(2, average="micro", threshold=0.3)(y_true, y_pred),
-            elegy.metrics.F1(threshold=0.3)(
-                jnp.asarray(y_true), jnp.asarray(y_pred)
-            ),
+            elegy.metrics.F1(threshold=0.3)(jnp.asarray(y_true), jnp.asarray(y_pred)),
         )
 
         assert np.allclose(
-            tfa.metrics.F1Score(2, average="micro", threshold=0.3)(y_true, y_pred, sample_weight=sample_weight),
+            tfa.metrics.F1Score(2, average="micro", threshold=0.3)(
+                y_true, y_pred, sample_weight=sample_weight
+            ),
             elegy.metrics.F1(threshold=0.3)(
                 jnp.asarray(y_true), jnp.asarray(y_pred), sample_weight=sample_weight
             ),
--- elegy/metrics/mean_absolute_error_test.py
+++ elegy/metrics/mean_absolute_error_test.py
@@ -11,6 +11,7 @@ import elegy
 # debugpy.listen(5679)
 # debugpy.wait_for_client()
 
+
 class MAETest(TestCase):
     #
     def test_basic(self):
@@ -20,23 +21,21 @@ class MAETest(TestCase):
 
         assert np.allclose(
             elegy.metrics.MeanAbsoluteError()(y_true, y_pred),
-            tfk.metrics.MeanAbsoluteError()(y_true, y_pred)
-            )
+            tfk.metrics.MeanAbsoluteError()(y_true, y_pred),
+        )
 
-        y_true=jnp.array([1, 1, 1, 1])
-        y_pred=jnp.array([1, 0, 0, 0])
+        y_true = jnp.array([1, 1, 1, 1])
+        y_pred = jnp.array([1, 0, 0, 0])
 
         assert np.allclose(
             elegy.metrics.MeanAbsoluteError()(y_true, y_pred),
-            tfk.metrics.MeanAbsoluteError()(y_true, y_pred)
-            )
+            tfk.metrics.MeanAbsoluteError()(y_true, y_pred),
+        )
 
         y_true = (np.random.uniform(0, 1, size=(5, 6, 7)) > 0.5).astype(np.int32)
         y_pred = np.random.uniform(0, 1, size=(5, 6, 7))
 
         assert np.allclose(
             tfk.metrics.MeanAbsoluteError()(y_true, y_pred),
-            elegy.metrics.MeanAbsoluteError()(
-                jnp.asarray(y_true), jnp.asarray(y_pred)
-            ),
+            elegy.metrics.MeanAbsoluteError()(jnp.asarray(y_true), jnp.asarray(y_pred)),
         )
