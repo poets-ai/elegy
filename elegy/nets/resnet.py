3840208eae02b49a464d3228dacfa5f9f529a536
alexander-g <3867427+alexander-g@users.noreply.github.com>
2020-12-05 14:15:19 +0100

    Low resolution version of ResNet

--- elegy/nets/resnet.py
+++ elegy/nets/resnet.py
@@ -7,44 +7,65 @@ from elegy import module, nn
 class ResNetBlock(module.Module):
     """ResNet (identity) block"""
 
-    def call(self, x, n_filters, strides=(1, 1)):
+    def __init__(self, n_filters, strides=(1, 1), *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.n_filters = n_filters
+        self.strides = strides
+
+    def call(self, x):
         x0 = x
         x = nn.Conv2D(
-            n_filters, (3, 3), with_bias=False, stride=strides, dtype=self.dtype
+            self.n_filters,
+            (3, 3),
+            with_bias=False,
+            stride=self.strides,
+            dtype=self.dtype,
         )(x)
         x = nn.BatchNormalization(decay_rate=0.9, eps=1e-5)(x)
         x = jax.nn.relu(x)
 
-        x = nn.Conv2D(n_filters, (3, 3), with_bias=False, dtype=self.dtype)(x)
+        x = nn.Conv2D(self.n_filters, (3, 3), with_bias=False, dtype=self.dtype)(x)
         x = nn.BatchNormalization(decay_rate=0.9, eps=1e-5)(x)
 
         if x0.shape != x.shape:
             x0 = nn.Conv2D(
-                n_filters, (1, 1), with_bias=False, stride=strides, dtype=self.dtype
+                self.n_filters,
+                (1, 1),
+                with_bias=False,
+                stride=self.strides,
+                dtype=self.dtype,
             )(x0)
             x0 = nn.BatchNormalization(decay_rate=0.9, eps=1e-5)(x0)
         return jax.nn.relu(x0 + x)
 
 
-class BottleneckResNetBlock(module.Module):
+class BottleneckResNetBlock(ResNetBlock):
     """ResNet Bottleneck block."""
 
-    def call(self, x, n_filters, strides=(1, 1)):
+    def call(self, x):
         x0 = x
-        x = nn.Conv2D(n_filters, (1, 1), with_bias=False, dtype=self.dtype)(x)
+        x = nn.Conv2D(self.n_filters, (1, 1), with_bias=False, dtype=self.dtype)(x)
         x = nn.BatchNormalization(decay_rate=0.9, eps=1e-5)(x)
         x = jax.nn.relu(x)
         x = nn.Conv2D(
-            n_filters, (3, 3), with_bias=False, stride=strides, dtype=self.dtype
+            self.n_filters,
+            (3, 3),
+            with_bias=False,
+            stride=self.strides,
+            dtype=self.dtype,
         )(x)
         x = nn.BatchNormalization(decay_rate=0.9, eps=1e-5)(x)
         x = jax.nn.relu(x)
-        x = nn.Conv2D(n_filters * 4, (1, 1), with_bias=False, dtype=self.dtype)(x)
+        x = nn.Conv2D(self.n_filters * 4, (1, 1), with_bias=False, dtype=self.dtype)(x)
         x = nn.BatchNormalization(decay_rate=0.9, eps=1e-5, scale_init=jnp.zeros)(x)
 
         if x0.shape != x.shape:
             x0 = nn.Conv2D(
-                n_filters * 4, (1, 1), with_bias=False, stride=strides, dtype=self.dtype
+                self.n_filters * 4,
+                (1, 1),
+                with_bias=False,
+                stride=self.strides,
+                dtype=self.dtype,
             )(x0)
             x0 = nn.BatchNormalization(decay_rate=0.9, eps=1e-5)(x0)
         return jax.nn.relu(x0 + x)
@@ -53,28 +74,37 @@ class BottleneckResNetBlock(module.Module):
 class ResNet(module.Module):
     """ResNet V1"""
 
-    def __init__(self, stages, block_type, *args, **kwargs):
+    def __init__(self, stages, block_type, lowres=False, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.stages = stages
         self.block_type = block_type
+        self.lowres = lowres
 
     def call(self, x):
         x = nn.Conv2D(
-            64, (7, 7), stride=(2, 2), padding="SAME", with_bias=False, dtype=self.dtype
+            64, 
+            (7, 7) if not self.lowres else (3,3), 
+            stride=(2, 2) if not self.lowres else (1,1), 
+            padding="SAME", 
+            with_bias=False, 
+            dtype=self.dtype
         )(x)
         x = nn.BatchNormalization(decay_rate=0.9, eps=1e-5)(x)
-        x = jax.nn.relu(x)
+        x = module.to_module(jax.nn.relu)()(x)
 
-        x = nn.linear.hk.max_pool(
-            x, window_shape=(1, 3, 3, 1), strides=(1, 2, 2, 1), padding="SAME"
-        )
+        if not self.lowres:
+            x = nn.MaxPool(window_shape=(1, 3, 3, 1), strides=(1, 2, 2, 1), padding="SAME")(
+                x
+            )
         for i, block_size in enumerate(self.stages):
             for j in range(block_size):
                 strides = (2, 2) if i > 0 and j == 0 else (1, 1)
-                x = self.block_type(dtype=self.dtype)(x, 64 * 2 ** i, strides=strides)
-        x = jnp.mean(x, axis=(1, 2))
+                x = self.block_type(64 * 2 ** i, strides=strides, dtype=self.dtype)(x)
+        GAP = lambda x: jnp.mean(x, axis=(1, 2))
+        x = module.to_module(GAP)(name="global_average_pooling")(x)
         x = nn.Linear(1000, dtype=self.dtype)(x)
-        x = jnp.asarray(x, jnp.float32)
+        to_float32 = lambda x: jnp.asarray(x, jnp.float32)
+        x = module.to_module(to_float32)(name="to_float32")(x)
         return x
 
 
@@ -94,20 +124,17 @@ class ResNet50(ResNet):
             stages=[3, 4, 6, 3], block_type=BottleneckResNetBlock, *args, **kwargs
         )
 
-
 class ResNet101(ResNet):
     def __init__(self, *args, **kwargs):
         super().__init__(
             stages=[3, 4, 23, 3], block_type=BottleneckResNetBlock, *args, **kwargs
         )
 
-
 class ResNet152(ResNet):
     def __init__(self, *args, **kwargs):
         super().__init__(
             stages=[3, 8, 36, 3], block_type=BottleneckResNetBlock, *args, **kwargs
         )
-
 
 class ResNet200(ResNet):
     def __init__(self, *args, **kwargs):
