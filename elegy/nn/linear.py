4f6c9688697fc06aab3925f08c66e30e0029f7e6
Cristian Garcia <cgarcia.e88@gmail.com>
2020-12-18 11:56:21 -0500

    [WIP] update linear

--- elegy/nn/linear.py
+++ elegy/nn/linear.py
@@ -148,67 +148,143 @@ class LinearGeneral(module.Module):
 
 
 class Linear(module.Module):
-    """Linear module."""
+    """
+    Just your regular densely-connected NN layer.
+
+    `Linear` implements the operation:
+
+    ```python
+    output = activation(dot(input, kernel) + bias)
+    ```
+
+    where `activation` is the element-wise activation function
+    passed as the `activation` argument, `kernel` is a weights matrix
+    created by the layer, and `bias` is a bias vector created by the layer
+    (only applicable if `use_bias` is `True`).
+
+    Note: If the input to the layer has a rank greater than 2, then `Linear`
+    computes the dot product between the `inputs` and the `kernel` along the
+    last axis of the `inputs` and axis 1 of the `kernel` (using `tf.tensordot`).
+    For example, if input has dimensions `(batch_size, d0, d1)`,
+    then we create a `kernel` with shape `(d1, units)`, and the `kernel` operates
+    along axis 2 of the `input`, on every sub-tensor of shape `(1, 1, d1)`
+    (there are `batch_size * d0` such sub-tensors).
+
+    The output in this case will have shape `(batch_size, d0, units)`.
+    Besides, layer attributes cannot be modified after the layer has been called
+    once (except the `trainable` attribute).
+
+    Example:
+
+    >>> # Create a `Sequential` model and add a Linear layer as the first layer.
+    >>> model = tf.keras.models.Sequential()
+    >>> model.add(tf.keras.Input(shape=(16,)))
+    >>> model.add(tf.keras.layers.Linear(32, activation='relu'))
+    >>> # Now the model will take as input arrays of shape (None, 16)
+    >>> # and output arrays of shape (None, 32).
+    >>> # Note that after the first layer, you don't need to specify
+    >>> # the size of the input anymore:
+    >>> model.add(tf.keras.layers.Linear(32))
+    >>> model.output_shape
+    >>> (None, 32)
+    """
 
     w: np.ndarray
     b: np.ndarray
 
     def __init__(
         self,
-        output_size: int,
-        with_bias: bool = True,
-        w_init: tp.Optional[Initializer] = None,
-        b_init: tp.Optional[Initializer] = None,
-        **kwargs
+        units: int,
+        use_bias: bool = True,
+        kernel_initializer: Initializer = VarianceScaling(),
+        bias_initializer: Initializer = Zeros(),
+        kernel_regularizer: tp.Optional[tp.Callable] = None,
+        bias_regularizer: tp.Optional[tp.Callable] = None,
+        activity_regularizer: tp.Optional[tp.Callable] = None,
+        kernel_constraint: tp.Optional[tp.Callable] = None,
+        bias_constraint: tp.Optional[tp.Callable] = None,
+        precision: tp.Any = None,
+        **kwargs,
     ):
         """
-        Constructs the Linear module.
 
         Arguments:
-            output_size: Output dimensionality.
-            with_bias: Whether to add a bias to the output.
-            w_init: Optional initializer for weights. By default, uses random values
-                from truncated normal, with stddev `1 / sqrt(fan_in)`. See
-                https://arxiv.org/abs/1502.03167v3.
-            b_init: Optional initializer for bias. By default, zero.
-            kwargs: Additional keyword arguments passed to Module.
+            units: Positive integer, dimensionality of the output space.
+            activation: Activation function to use.
+                If you don't specify anything, no activation is applied
+                (ie. "linear" activation: `a(x) = x`).
+            use_bias: Boolean, whether the layer uses a bias vector.
+            kernel_initializer: Initializer for the `kernel` weights matrix.
+            bias_initializer: Initializer for the bias vector.
+            kernel_regularizer: Regularizer function applied to
+                the `kernel` weights matrix.
+            bias_regularizer: Regularizer function applied to the bias vector.
+            activity_regularizer: Regularizer function applied to
+                the output of the layer (its "activation").
+            kernel_constraint: Constraint function applied to
+                the `kernel` weights matrix.
+            bias_constraint: Constraint function applied to the bias vector.
+            precision: numerical precision of the computation see `jax.lax.Precision`
+                for details.
+
+        Input shape:
+            N-D tensor with shape: `(batch_size, ..., input_dim)`.
+            The most common situation would be
+            a 2D input with shape `(batch_size, input_dim)`.
+
+        Output shape:
+            N-D tensor with shape: `(batch_size, ..., units)`.
+            For instance, for a 2D input with shape `(batch_size, input_dim)`,
+            the output would have shape `(batch_size, units)`.
         """
-        super().__init__(**kwargs)
-        self.input_size = None
-        self.output_size = output_size
-        self.with_bias = with_bias
-        self.w_init = w_init
-        self.b_init = b_init or jnp.zeros
+        super().__init__(activity_regularizer=activity_regularizer, **kwargs)
+
+        # self.input_size = None
+        # self.output_size = output_size
+        # self.with_bias = with_bias
+        # self.w_init = w_init
+        # self.b_init = b_init or jnp.zeros
+
+        self.units = units
+        self.use_bias = use_bias
+        self.kernel_initializer = kernel_initializer
+        self.bias_initializer = bias_initializer
+        self.kernel_regularizer = kernel_regularizer
+        self.bias_regularizer = bias_regularizer
+        self.activity_regularizer = activity_regularizer
+        self.kernel_constraint = kernel_constraint
+        self.bias_constraint = bias_constraint
+        self.precision = precision
 
     def call(self, inputs: np.ndarray) -> np.ndarray:
         """"""
         if not inputs.shape:
             raise ValueError("Input must not be scalar.")
 
-        input_size = self.input_size = inputs.shape[-1]
-        output_size = self.output_size
-        dtype = jnp.float32
-
-        w_init = self.w_init
-
-        if w_init is None:
-            stddev = 1.0 / np.sqrt(self.input_size)
-            w_init = TruncatedNormal(stddev=stddev)
-
-        w = self.add_parameter(
-            "w", [input_size, output_size], dtype, initializer=w_init
-        )
-
         inputs = jnp.asarray(inputs, self.dtype)
-        w = jnp.asarray(w, self.dtype)
-        out = jnp.dot(inputs, w)
 
-        if self.with_bias:
-            b = self.add_parameter(
-                "b", [self.output_size], dtype, initializer=self.b_init
+        kernel = self.add_parameter(
+            name="kernel",
+            shape=(inputs.shape[-1], self.units),
+            initializer=self.kernel_initializer,
+            trainable=True,
+        )
+        kernel = jnp.asarray(kernel, self.dtype)
+
+        y = lax.dot_general(
+            inputs,
+            kernel,
+            (((inputs.ndim - 1,), (0,)), ((), ())),
+            precision=self.precision,
+        )
+        if self.use_bias:
+            bias = self.add_parameter(
+                name="bias",
+                shape=(self.units,),
+                initializer=self.bias_initializer,
+                trainable=True,
             )
-            b = jnp.broadcast_to(b, out.shape)
-            b = jnp.asarray(b, self.dtype)
-            out = out + b
+            bias = jnp.asarray(bias, self.dtype)
+            y = y + bias
 
-        return out
+        return y
