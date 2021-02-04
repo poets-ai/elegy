da818f33fc64c44899d38930c18bfa9625841a81
alexander-g <3867427+alexander-g@users.noreply.github.com>
2021-02-04 08:26:59 +0100

    README + black

--- examples/WGAN-GP/main.py
+++ examples/WGAN-GP/main.py
@@ -1,11 +1,10 @@
-import os, glob
+import os, glob, pickle
 import numpy as np
 from absl import flags, app
-import PIL
+import PIL.Image
 
 import elegy
 from model import WGAN_GP
-
 
 
 FLAGS = flags.FLAGS
@@ -26,22 +25,20 @@ flags.mark_flag_as_required("dataset")
 flags.mark_flag_as_required("output_dir")
 
 
-
-
 class Dataset(elegy.data.Dataset):
     def __init__(self, path):
         self.files = glob.glob(os.path.expanduser(path))
-        if len(self.files)==0:
+        if len(self.files) == 0:
             raise RuntimeError(f'Could not find any files in path "{path}"')
-        print(f'Found {len(self.files)} files')
-    
+        print(f"Found {len(self.files)} files")
+
     def __len__(self):
         return len(self.files)
-    
+
     def __getitem__(self, i):
-        f          = self.files[i]
-        img        = np.array(PIL.Image.open(f).resize((64,64))) / np.float32(255)
-        img        = np.fliplr(img) if np.random.random()<0.5 else img
+        f = self.files[i]
+        img = np.array(PIL.Image.open(f).resize((64, 64))) / np.float32(255)
+        img = np.fliplr(img) if np.random.random() < 0.5 else img
         return img
 
 
@@ -49,13 +46,23 @@ class SaveImagesCallback(elegy.callbacks.Callback):
     def __init__(self, model, path):
         self.model = model
         self.path = path
+
     def on_epoch_end(self, epoch, *args, **kwargs):
-        x = self.model.predict(np.random.normal(size=[8,128]))
-        x = np.concatenate(list(x*255), axis=1).astype(np.uint8)
+        x = self.model.predict(np.random.normal(size=[8, 128]))
+        x = np.concatenate(list(x * 255), axis=1).astype(np.uint8)
         img = PIL.Image.fromarray(x)
-        img.save(os.path.join(self.path, f'epoch-{epoch:04d}.png'))
+        img.save(os.path.join(self.path, f"epoch-{epoch:04d}.png"))
 
 
+class SaveStatesCallback(elegy.callbacks.Callback):
+    def __init__(self, model, path):
+        self.model = model
+        self.path = path
+
+    def on_epoch_end(self, *args, **kwargs):
+        open(os.path.join(self.path, "modelstates.pkl"), "wb").write(
+            pickle.dumps(self.model.states)
+        )
 
 
 def main(argv):
@@ -68,20 +75,24 @@ def main(argv):
     ), "Output directory already exists. Delete manually or specify a new one."
     os.makedirs(FLAGS.output_dir)
 
-
-
     ds = Dataset(FLAGS.dataset)
-    loader = elegy.data.DataLoader(ds, batch_size=FLAGS.batch_size, n_workers=os.cpu_count(), worker_type='process')
+    loader = elegy.data.DataLoader(
+        ds, batch_size=FLAGS.batch_size, n_workers=os.cpu_count(), worker_type="process"
+    )
 
     wgan = WGAN_GP()
-    wgan.states = wgan.init( np.zeros([8,128]) )
+    wgan.states = wgan.init(np.zeros([8, 128]))
 
     wgan.fit(
         loader,
         epochs=FLAGS.epochs,
         verbose=4,
-        callbacks=[SaveImagesCallback(wgan, FLAGS.output_dir)]
+        callbacks=[
+            SaveImagesCallback(wgan, FLAGS.output_dir),
+            SaveStatesCallback(wgan, FLAGS.output_dir),
+        ],
     )
+
 
 if __name__ == "__main__":
     app.run(main)
